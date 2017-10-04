fb28945ab0e09895bc4cbb223b7f452c56e31dcd
Fedor Baart <fedor.baart@deltares.nl>
2017-10-04 16:20:16 +0200

    cors

--- stathakis/app.py
+++ stathakis/app.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
 
 import connexion
+import flask_cors
 
 
 def make_app():
     app = connexion.App(__name__, specification_dir='./swagger/')
     app.add_api('swagger.yaml', arguments={'title': 'This API allows you to retrieve realtime and historic measurements. It is a frontend for measurements from other sources. It is intended for stations or gridded timeseries.'})
+    # add CORS to the app, should not have any secure api's
+    flask_cors.CORS(app.app)
     return app
